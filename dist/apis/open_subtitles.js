55558ba813ac6816d78ccfc27789cf19d14da2b6
Arthur Mialon <arthurmialon@gmail.com>
2015-11-17 14:37:02 +0100

    Build dist

--- dist/apis/open_subtitles.js
+++ dist/apis/open_subtitles.js
@@ -57,8 +57,8 @@ exports.default = function () {
 
     /* Necessary parameters */
     parameters: {
-      'serie': ['languages', 'imdbId', 'season', 'episode', 'fileName', 'filePath'],
-      'movie': ['languages', 'imdbId', 'fileName', 'filePath']
+      serie: ['languages', 'imdbId', 'season', 'episode', 'fileName', 'filePath'],
+      movie: ['languages', 'imdbId', 'fileName', 'filePath']
     },
 
     /**
