ff5f35cad84247f154fc2b70e3f134d278d37b60
Mathieu <mathieu.letyrant@gmail.com>
2015-11-17 13:28:02 +0100

    First step for openSubtitlesApi

--- dist/apis/open_subtitles.js
+++ dist/apis/open_subtitles.js
@@ -3,34 +3,98 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+
+var _opensubtitlesApi = require('opensubtitles-api');
+
+var _opensubtitlesApi2 = _interopRequireDefault(_opensubtitlesApi);
+
+var _q = require('q');
+
+var _q2 = _interopRequireDefault(_q);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 /**
  * Export Open Subtitles API helper
  */
 
 exports.default = function () {
-  /* Here private methods and private properties */
+
+  /* Define apiName */
+  var apiName = 'open-subtitles';
+
+  /* Define opensubtitles-api instance */
+  var openSubtitlesApi = new _opensubtitlesApi2.default('OSTestUserAgent');
+
+  /* Format openSubtitles format to Dragand format */
+  var formatJson = function formatJson(result) {
+
+    var resultFormated = [];
+
+    options.languages.forEach(function (language) {
+      Object.keys(result).forEach(function (index) {
+        if (index === language) {
+          resultFormated.push({
+            type: result[index].url.substr(result[index].url.lastIndexOf('.') + 1),
+            language: result[index].lang,
+            url: result[index].url,
+            api: apiName
+          });
+        }
+      });
+    });
+
+    return resultFormated;
+  };
+
+  /* Options */
+  var options = {};
 
   return {
     /* Name of the api */
-    name: 'open-subtitle',
+    name: apiName,
 
     /* Type of data available */
-    type: ['movie', 'serie'],
+    type: ['serie', 'movie'],
 
     /* Necessary parameters */
-    parameters: [],
+    parameters: {
+      'serie': ['languages', 'imdbId', 'season', 'episode', 'fileName', 'filePath'],
+      'movie': ['languages', 'imdbId', 'fileName', 'filePath']
+    },
 
     /**
      * Set options before call
      * @param {object} configuration
      * @return {boolean} is configuration is valid
      */
-    setOptions: function setOptions() {},
+    setOptions: function setOptions() {
+      var opts = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+
+      options = opts;
+    },
 
     /**
      * Calling the API
      * @return {promise} format data and errors
      */
-    call: function call() {}
+    callSeries: function callSeries() {
+      var deferred = _q2.default.defer();
+
+      openSubtitlesApi.search({
+        sublanguageid: 'all',
+        imdbid: options.imdbId,
+        season: options.season,
+        episode: options.episode,
+        filename: options.fileName,
+        path: options.filePath
+      }).then(function (subtitles) {
+        deferred.resolve(formatJson(subtitles));
+      }).catch(function (error) {
+        deferred.reject(error);
+      });
+
+      return deferred.promise;
+    }
   };
 };
--- src/apis/open_subtitles.js
+++ src/apis/open_subtitles.js
@@ -1,34 +1,85 @@
+import os from 'opensubtitles-api';
+import Q from 'q';
+
 /**
  * Export Open Subtitles API helper
  */
 export default () => {
-  /* Here private methods and private properties */
+
+  /* Define apiName */
+  const apiName = 'open-subtitles';
+
+  /* Define opensubtitles-api instance */
+  const openSubtitlesApi = new os('OSTestUserAgent');
+
+  /* Format openSubtitles format to Dragand format */
+  const formatJson = (result) => {
+
+    let resultFormated = [];
+
+    options.languages.forEach((language) => {
+      Object.keys(result).forEach((index) => {
+        if(index === language) {
+          resultFormated.push({
+            type: result[index].url.substr(result[index].url.lastIndexOf('.') + 1),
+            language: result[index].lang,
+            url: result[index].url,
+            api: apiName
+          });
+        }
+      });
+    });
+
+    return resultFormated;
+
+  };
+
+  /* Options */
+  let options = {};
 
   return {
     /* Name of the api */
-    name: 'open-subtitle',
+    name: apiName,
 
     /* Type of data available */
-    type: ['movie', 'serie'],
+    type: ['serie', 'movie'],
 
     /* Necessary parameters */
-    parameters: [
-
-    ],
+    parameters: {
+      'serie': [ 'languages', 'imdbId', 'season', 'episode', 'fileName', 'filePath' ],
+      'movie': [ 'languages', 'imdbId', 'fileName', 'filePath' ]
+    },
 
     /**
      * Set options before call
      * @param {object} configuration
      * @return {boolean} is configuration is valid
      */
-    setOptions() {
+    setOptions(opts = {}) {
+      options = opts;
     },
 
     /**
      * Calling the API
      * @return {promise} format data and errors
      */
-    call() {
+    callSeries() {
+      let deferred = Q.defer();
+
+      openSubtitlesApi.search({
+        sublanguageid: 'all',
+        imdbid: options.imdbId,
+        season: options.season,
+        episode: options.episode,
+        filename: options.fileName,
+        path: options.filePath
+      }).then(subtitles => {
+        deferred.resolve(formatJson(subtitles));
+      }).catch(error => {
+        deferred.reject(error);
+      });
+
+      return deferred.promise;
     }
 
   };
